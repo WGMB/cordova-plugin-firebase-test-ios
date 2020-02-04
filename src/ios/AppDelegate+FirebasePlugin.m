cb72528ecca348ad9311bcb99aa7b53a586f2861
WGMB <41430264+WGMB@users.noreply.github.com>
2020-02-04 17:35:50 +0200

    Update AppDelegate+FirebasePlugin.m

--- src/ios/AppDelegate+FirebasePlugin.m
+++ src/ios/AppDelegate+FirebasePlugin.m
@@ -97,25 +97,25 @@
     // Note that this callback will be fired everytime a new token is generated, including the first
     // time. So if you need to retrieve the token as soon as it is available this is where that
     // should be done.
-    NSString *refreshedToken = [[FIRInstanceID instanceID] token];
+    NSString *refreshedToken = [FIRMessaging messaging].FCMToken;
     NSLog(@"InstanceID token: %@", refreshedToken);
 
     // Connect to FCM since connection may have failed when attempted before having a token.
-    [self connectToFcm];
+    //[self connectToFcm];
     [FirebasePlugin.firebasePlugin sendToken:refreshedToken];
 }
 
-- (void)connectToFcm {
-    [[FIRMessaging messaging] connectWithCompletion:^(NSError * _Nullable error) {
-        if (error != nil) {
-            NSLog(@"Unable to connect to FCM. %@", error);
-        } else {
-            NSLog(@"Connected to FCM.");
-            NSString *refreshedToken = [[FIRInstanceID instanceID] token];
-            NSLog(@"InstanceID token: %@", refreshedToken);
-        }
-    }];
-}
+// - (void)connectToFcm {
+//     [[FIRMessaging messaging] connectWithCompletion:^(NSError * _Nullable error) {
+//         if (error != nil) {
+//             NSLog(@"Unable to connect to FCM. %@", error);
+//         } else {
+//             NSLog(@"Connected to FCM.");
+//             NSString *refreshedToken = [FIRMessaging messaging].FCMToken;
+//             NSLog(@"InstanceID token: %@", refreshedToken);
+//         }
+//     }];
+// }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     [FIRMessaging messaging].APNSToken = deviceToken;
